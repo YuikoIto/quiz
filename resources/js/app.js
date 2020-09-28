f13ce1bcfec267747878b2b0987ebfd04fa4f998
YuikoIto <toffywing@gmail.com>
2020-09-29 02:35:50 +0900

    vueファイルをlaravelに移行

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import Vue from 'vue';
+import App from './components/page/Home';
+import SocialSharing from 'vue-social-sharing';
+
+Vue.use(SocialSharing);
+
+new Vue({
+  el: 'app',
+  components: {
+    app: App,
+  },
+});
