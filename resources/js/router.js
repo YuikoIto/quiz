9c71417c06438bd2c43ebaa33cfcef5ae937637e
YuikoIto <toffywing@gmail.com>
2020-09-30 16:22:24 +0900

    enable to route by keyword

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./components/page/Home";
 import Quiz from "./components/page/Quiz";
-import Register from "./components/page/Register";
 import Login from "./components/page/Login";
+import Register from "./components/page/Register";
 import Mypage from "./components/page/Mypage";
+import Keyword from "./components/page/Keyword";
 
 Vue.use(Router);
 
@@ -22,19 +23,24 @@ export default new Router({
             component: Quiz
         },
         {
-            path: "/register",
-            name: "register",
-            component: Register
-        },
-        {
             path: "/login",
             name: "login",
             component: Login
         },
         {
+            path: "/register",
+            name: "register",
+            component: Register
+        },
+        {
             path: "/mypage",
             name: "mypage",
             component: Mypage
+        },
+        {
+            path: "/keyword",
+            name: "keyword",
+            component: Keyword
         }
     ]
 });
